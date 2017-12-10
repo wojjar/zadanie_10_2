945179b94bd1d511bb42b2e915628f72f7581977
Jarosław Wójcik <j.wojcik@interia.eu>
2017-12-10 15:16:23 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,16 @@
 var span = $("span");
     span.each(function(index, element) {
 });
-
 span.each(function(index, element) {
 	if(index % 2 == 0) {
 		$(element).css('color', 'red');
 	};
 });
-
 var paragraphs = $('p');
     paragraphs.each(function(index, element) {
     	var button = '<button class="btn" data-tmp=" ' + index + ' ">Click me</button>'
    		 $(element).append(button)
 });
-
 $("button").click(function() {
 	alert(("This button has an index ")+$(this).attr("data-tmp")+("."));
 });
